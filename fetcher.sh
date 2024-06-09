18f3b2e2c0cf01e6e15d4ea0bc59268d052d430a
Nakaya <eniehack@outlook.jp>
2024-06-09 23:12:10 +0900

    fix: remove admin_token

--- fetcher.sh
+++ fetcher.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-MISSKEY_TOKEN="Rv0WhSOwv2Fa8PRO30I7aOcMfeK4v8n9"
-MISSKEY_HOST="misskey.until.tsukuba.one"
+ADMIN_TOKEN="${MISSKEY_ADMIN_TOKEN}"
+MKHOST="${MISSKEY_HOST=misskey.until.tsukuba.one}"
 POETRY=$(which poetry)
 
 USERID_LIST_FILE=$(mktemp)
 echo "USERID_LIST_FILE: $USERID_LIST_FILE" 1>&2
-$POETRY run python3 src/fetchuserlists.py --host "$MISSKEY_HOST" --token "$MISSKEY_TOKEN" \
+"$POETRY" run python3 src/fetchuserlists.py --host "$MKHOST" --token "$ADMIN_TOKEN" \
     | python3 -c 'import sys,json; d=json.load(sys.stdin); [print(i["id"]) for i in d]' \
     > "$USERID_LIST_FILE"
 
 NOTEID_LIST_FILE=$(mktemp)
 echo "NOTEID_LIST_FILE: $NOTEID_LIST_FILE" 1>&2
 cat "$USERID_LIST_FILE"  \
-    | xargs -I {} -P 4 -t sh -c 'sleep 1 && '"$POETRY"' run python3 src/fetchusernotes.py -u {} --host '"$MISSKEY_HOST"' >> '"$NOTEID_LIST_FILE"
-"$POETRY" run python3 src/filternotes.py "$NOTEID_LIST_FILE" | xargs -I {} -P 4 echo "https://$MISSKEY_HOST/notes/"{}
+    | xargs -I {} -P 4 -t sh -c 'sleep 1 && '"$POETRY"' run python3 src/fetchusernotes.py -u {} --host '"$MKHOST"' >> '"$NOTEID_LIST_FILE"
+"$POETRY" run python3 src/filternotes.py "$NOTEID_LIST_FILE" | xargs -I {} -P 4 echo "https://$MKHOST/notes/"{}
         #"$POETRY"' run python3 src/fetchusernotes.py -u {} --host '"$MISSKEY_HOST"' | '"$POETRY"' run python3 -c '\''import sys,json; print(json.load(sys.stdin))'\'
         #"$POETRY"' run python3 src/fetchusernotes.py -u {} --host '"$MISSKEY_HOST"
 #while read user; do
